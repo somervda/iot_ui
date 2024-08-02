a2906aa3b0b5c7a69093a9bca5b7427f0b7d86c0
David SurfacePro <somervda@comcast.net>
2024-08-02 13:24:28 -0400

    wip: status

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+
+import { FormsModule } from '@angular/forms';
+import {
+  MeasurementsService,
+  Application,
+  Device,
+  MeasurementQuery,
+} from '../services/measurements.service';
 
 @Component({
   selector: 'app-status',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatSelectModule, FormsModule],
   templateUrl: './status.component.html',
   styleUrl: './status.component.scss'
 })
 export class StatusComponent {
+  devices$$: Subscription | undefined;
+  devices: Device[] | undefined;
+  device_id = 1;
+
+  constructor(private measurmentsService: MeasurementsService) {
+    this.loadDevices();
+  }
+
+  loadDevices() {
+    // get the array of available result files
+    this.devices$$ = this.measurmentsService
+      .getDevices()
+      .subscribe((devices) => {
+        console.log(devices);
+        this.devices = devices;
+      });
+  }
 
 }
