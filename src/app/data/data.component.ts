9d43728b61c7cf22ea62c419726eae9a4c899183
David <39265971+somervda@users.noreply.github.com>
2024-08-11 17:04:05 -0400

    wip: adding mat-table

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -7,23 +7,26 @@ import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { SelectorComponent } from '../selector/selector.component';
 import { Subscription } from 'rxjs';
-
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-data',
   standalone: true,
-  imports: [ 
+  imports: [
     MatCardModule,
     MatExpansionModule,
-    SelectorComponent],
+    SelectorComponent,
+    MatTableModule,
+  ],
   templateUrl: './data.component.html',
-  styleUrl: './data.component.scss'
+  styleUrl: './data.component.scss',
 })
 export class DataComponent {
   measurements$$: Subscription | undefined;
   displayData = false;
   resultsAsString = '';
-
+  results = [];
+  columns = [{ title: 'id' }];
 
   constructor(private measurementService: MeasurementsService) {}
 
@@ -35,23 +38,22 @@ export class DataComponent {
   getData(measurementQuery: MeasurementQuery) {
     console.log('getData:', measurementQuery);
     if (measurementQuery) {
-        this.measurements$$ = this.measurementService
-          .getFlatMeasurements(
-            measurementQuery.application_id,
-            measurementQuery.device_id,
-            measurementQuery.umt,
-            measurementQuery.rows,
-            measurementQuery.grouping
-          )
-          .subscribe((results) => {
-            console.log('getFlatMeasurements', results);
-            this.resultsAsString = JSON.stringify(results);
-            this.displayData = true;
-          });
-      } else {
-        this.displayData = false;
-      }
+      this.measurements$$ = this.measurementService
+        .getFlatMeasurements(
+          measurementQuery.application_id,
+          measurementQuery.device_id,
+          measurementQuery.umt,
+          measurementQuery.rows,
+          measurementQuery.grouping
+        )
+        .subscribe((results) => {
+          console.log('getFlatMeasurements', results);
+          this.resultsAsString = JSON.stringify(results);
+          this.results = results;
+          this.displayData = true;
+        });
+    } else {
+      this.displayData = false;
     }
-
-
+  }
 }
