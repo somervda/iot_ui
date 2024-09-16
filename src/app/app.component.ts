b13c6a572da962bdfcc61679c7e34460b5fe8e54
David <39265971+somervda@users.noreply.github.com>
2024-09-16 17:29:45 -0400

    working with routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,33 +3,54 @@ import { RouterOutlet } from '@angular/router';
 import { ChartComponent } from './chart/chart.component';
 import { DataComponent } from './data/data.component';
 import { MatTabsModule } from '@angular/material/tabs';
-import { StatusComponent } from "./status/status.component";
+import { StatusComponent } from './status/status.component';
 import { HttpParams } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,
+  imports: [
+    RouterOutlet,
     ChartComponent,
     MatTabsModule,
     StatusComponent,
-    DataComponent],
+    DataComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'iot_ui';
+  tabIndex = 0;
   constructor() {
     let url = window.location.href;
-    console.log("url",url)
+    console.log('url', url);
     let paramValue;
     if (url.includes('?')) {
       const httpParams = new HttpParams({ fromString: url.split('?')[1] });
-      console.log("httpParams",httpParams,httpParams.get("tab"))
+      console.log('httpParams', httpParams, httpParams.get('tab'));
+      if (httpParams.get('tab')) {
+        let tab = httpParams.get('tab');
+        console.log('tab:', tab);
+        switch (tab) {
+          case 'status': {
+            this.tabIndex = 0;
+            break;
+          }
+          case 'chart': {
+            this.tabIndex = 1;
+            break;
+          }
+          case 'data': {
+            this.tabIndex = 2;
+            break;
+          }
+          default: {
+            this.tabIndex = 0;
+            break;
+          }
+        }
+      }
     }
-
-
   }
-
 }
